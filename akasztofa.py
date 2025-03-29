15f9430b54e0a28a58e31eae9c07e2019e64aa2a
Indian1999 <haszab99@gmail.com>
2025-03-29 09:41:05 +0100

    join

--- akasztofa.py
+++ akasztofa.py
@@ -1,2 +1,37 @@
 import random
 puzzles = ["lion king", "finding nemo", "despicable me", "monster university", "frozen", "rio", "walle", "incerdibles", "sponge bob", "tom and jerry", "turning red", "finding dory", "shrek", "how to train your dragon", "inside out", "asterix and obelix", "the little mermaid", "cinderella", "tangled", "phineas and ferb", "kim possible", "samurai jack", "johny bravo", "pocahontas"]
+
+puzzle = puzzles[random.randrange(0, len(puzzles))]
+my_solution = ""
+for char in puzzle:
+    if char == " ":
+        my_solution += " "
+    else:
+        my_solution += "*"
+
+life = 10
+correct_letters = []
+wrong_letters = []
+
+while life > 0 and my_solution != puzzle: # Amíg tart a játék
+    print(my_solution)
+    tipp = input("Tippelj egy betűt, vagy add meg a megoldást!\n").lower()
+    if len(tipp) == 0:
+        continue # Ami a continue után jönne, az aktuális ciklus futásban, az kimarad és a következő ciklus iterációra ugrunk
+    elif len(tipp) == 1:
+        found = False
+        for i in range(len(puzzle)):
+            if puzzle[i] == tipp:
+                found = True
+                # Lista elemeit tudjuk módosítani, viszont string elemeket nem tudunk átítni (string[0] = f  NEM LEHET)
+                lista = list(my_solution) # ["*", "*", "a", "*", " ", "*", "f", "*"]
+                lista[i] = tipp
+                my_solution = "".join(lista) # A my_solution legyen egyenlő a lista elemeivel "" (üres string)-el elválasztva "b*a* *f*"
+        
+
+if life > 0:
+    print("Gratulálok, kitaláltad!")
+else:
+    print(f"Ez most nem jött össze. A megoldás ez volt: {puzzle}")
+
+
---
+++ join_metodus.py
@@ -0,0 +1,14 @@
+lista = ["asd", "3", "cica", "0", "kék"]
+
+string = " ".join(lista)
+print(string)
+
+string = "kiskutya".join(lista)
+print(string)
+
+string = "".join(lista)
+print(string)
+
+lista = ["*", "i", "*"]
+my_solution = "".join(lista)
+print(my_solution)
