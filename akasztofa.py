18750024b8be09328284e5a7d16ebc8da9c05106
Indian1999 <haszab99@gmail.com>
2025-03-29 09:08:38 +0100

    puzzles

---
+++ akasztofa.py
@@ -0,0 +1,2 @@
+import random
+puzzles = ["lion king", "finding nemo", "despicable me", "monster university", "frozen", "rio", "walle", "incerdibles", "sponge bob", "tom and jerry", "turning red", "finding dory", "shrek", "how to train your dragon", "inside out", "asterix and obelix", "the little mermaid", "cinderella", "tangled", "phineas and ferb", "kim possible", "samurai jack", "johny bravo", "pocahontas"]
