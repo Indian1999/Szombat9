3eb17191bcff54f292f938e2ae710e16ee12c51d
Indian1999 <haszab99@gmail.com>
2025-06-14 10:16:41 +0200

    2, 4, 12

--- szabaduloszoba.py
+++ szabaduloszoba.py
@@ -4,7 +4,8 @@ win = False
 solution = "4686"
 rooms = []
 items = []
-
+switches = "0000000"
+correct_switches = "1010011"
 
 print("""Üdvözöllek a szabadulószobában! 14 ajtót látsz magad előtt, ezek közül az egyik
 rejti a szabadságod kapuját! Találd ki a 4 számjegyű kódot, amivel ki
@@ -21,7 +22,10 @@ while gameOn:
     if room_number == "1":
         pass
     if room_number == "2":
-        pass
+        print(f"A falra 7 számjegy van felfestve: {correct_switches}")
+        if room_number not in rooms:
+            rooms.append(room_number)
+            items.append(f"számok a falon - {correct_switches}")
     if room_number == "3":
         print("""Ebben a szobában egy videó van kivetítve a falra, amin az látszik ahogy egy cicaeszik a
 táljából. Többszörös újranézés után meglátod, hogy táljára egy 2-es számjegy van írva""")
@@ -29,7 +33,15 @@ táljából. Többszörös újranézés után meglátod, hogy táljára egy 2-es
             rooms.append(room_number)
             items.append("cica eszik a 2-es tálból")
     if room_number == "4":
-        pass
+        print("Ebben a szobában egy tábla van, amire villanykörték vannak erősítve.")
+        if room_number not in rooms:
+            rooms.append(room_number)
+            items.append("tábla villanykörtékkel")
+        if switches == correct_switches:
+            print("Olyan mintha a villanykörték egy 6-os számjegyet rajzolnának ki.")
+            if "6-os villanykörték" not in items:
+                rooms.append(room_number)
+                items.append("6-os villanykörték")
     if room_number == "5": # x = 10111 - 10001    23 - 17 = 6
         print("Egy egyenlet van a falon: x = 10111 - 10001")
         print("Biztosan az x lesz az egyik számjegy...")
@@ -60,7 +72,11 @@ binárisból decimálisba. Ez az info még biztosan hasznos lesz...""")
             rooms.append(room_number)
             items.append("festmény - 2 bohóc kerget egy cicát")
     if room_number == "12":
-        pass
+        print("7 darab kapcsolót látsz a falon, de látszólag semmit nem csinálnak.")
+        if room_number not in rooms:
+            rooms.append(room_number)
+            items.append("7 kapcsoló a falon")
+        switches = input("Add meg, hogy milyen sorrendben akarod felkapcsolni a kapcsolókat (pl.: 0101111)\n")
     if room_number == "13":
         pass
     
