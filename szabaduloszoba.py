846e56992f9c40503f39237164db82a50a3a3348
Indian1999 <haszab99@gmail.com>
2025-06-14 09:22:15 +0200

    szabaduloszoba váza

---
+++ szabaduloszoba.py
@@ -0,0 +1,50 @@
+gameOn = True
+chances = 3 # 3 próbálkozásunk van a kód kitalálására
+win = False
+solution = "4686"
+rooms = []
+items = []
+
+
+print("""Üdvözöllek a szabadulószobában! 14 ajtót látsz magad előtt, ezek közül az egyik
+rejti a szabadságod kapuját! Találd ki a 4 számjegyű kódot, amivel ki
+tudsz jutni innen! Sok sikert!
+""")
+while gameOn:
+    room_number = input("Melyik szobába szeretnél bemenni? (0-13, -1)\n")
+    if room_number == "-1":
+        pass
+    if room_number == "0":
+        pass
+    if room_number == "1":
+        pass
+    if room_number == "2":
+        pass
+    if room_number == "3":
+        pass
+    if room_number == "4":
+        pass
+    if room_number == "5":
+        pass
+    if room_number == "6":
+        pass
+    if room_number == "7":
+        pass
+    if room_number == "8":
+        pass
+    if room_number == "9":
+        pass
+    if room_number == "10":
+        pass
+    if room_number == "11":
+        pass
+    if room_number == "12":
+        pass
+    if room_number == "13":
+        pass
+    
+    
+if win:
+    print("Kijutottál! Gratulálok!")
+else:
+    print("Ez most nem jött össze...")
