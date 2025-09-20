5a1521429d607e04714f8e28f081b5be06592910
Indian1999 <haszab99@gmail.com>
2025-09-20 09:28:24 +0200

    3. feladat kiírás

--- 2. félév/ismetles.py
+++ 2. félév/ismetles.py
@@ -1,7 +1,7 @@
 # 1. feladat: Olvassunk be egy számot és írjunk ki annyi #-t
 
-n = int(input("Adj meg egy számot: "))
-
+#n = int(input("Adj meg egy számot: "))
+n = 2
 for i in range(n):
     print("#", end="")
     
@@ -12,12 +12,18 @@ print("#" * n)
 # hogy létezhet-e ilyen háromszög!
 # Bármely két oldal összege legyen nagyobb a 3. oldalnál
 
-a = float(input("Add meg az a oldal hosszát: "))
-b = float(input("Add meg a b oldal hosszát: "))
-c = float(input("Add meg a c oldal hosszát: "))
-
+#a = float(input("Add meg az a oldal hosszát: "))
+#b = float(input("Add meg a b oldal hosszát: "))
+#c = float(input("Add meg a c oldal hosszát: "))
+a, b,c = 1,1,1
 if a + b > c and a + c > b and b + c > a:
     print("Ilyen háromszög létezik.")
 else:
     print("Nem létezik ilyen háromszög.")
     
+import random
+# 3. feladat: Adott egy egész számokat tartalmazó lista
+lista = [random.randint(-20, 20) for i in range(15)]
+print(lista)
+# a, Határozzuk meg a számok átlagát
+# b, Döntsük el, hogy van-e 11-el osztható szám a listában
