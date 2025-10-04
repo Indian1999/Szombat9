654921ea94fea87a4e8a1107d717364311fd98d0
Indian1999 <haszab99@gmail.com>
2025-10-04 09:24:46 +0200

    random

---
+++ 2. félév/modulok.py
@@ -0,0 +1,8 @@
+import random
+import math
+
+random.seed("almafa")
+print(random.randrange(1, 11)) # random szám 1-10 között
+print(random.randint(1,2))     # random szám 1-2 között
+print(random.randrange(0, 101, 5))  # random szám 1-100 között, 5 ösével
+
