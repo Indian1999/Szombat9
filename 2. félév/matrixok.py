26a33c84af94d55fb7a4fe6a51e7fb032505c25d
Indian1999 <haszab99@gmail.com>
2025-10-11 09:46:28 +0200

    eldöntés

--- 2. félév/matrixok.py
+++ 2. félév/matrixok.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot   # pip install matplotlib
 # Mátrix: Egy lista ami listákat tárol
 mtx = [
 #    0  1  2  3  4  5  6  7
-    [9, 0, 2, 9, 3, 8, 2, 3],   # 0    
-    [2, 8, 11, 3, 5, 5, 1, 2],  # 1 
-    [3, 0, 48, 2, 2, 8, 2, 7],  # 2 
-    [9, 1, 4, 0, 8, 9, 9, 0],   # 3
+    [9, 3, 2, 9, 3, 8, 2, 3],   # 0    
+    [2, 7, 11, 3, 5, 5, 1, 2],  # 1 
+    [3, 6, 48, 2, 2, 8, 2, 7],  # 2 
+    [9, 1, 4, 0, 8, 9, 9, 3],   # 3
     [8, 0, 44, 1, 2, 5, 2, 8]   # 4
 ]
 print(mtx)
@@ -39,3 +39,22 @@ for i in range(len(mtx)):
         if mtx[i][j] > maximum:
             maximum = mtx[i][j]
 print("A legnagyobb szám a mátrixban:", maximum)
+
+# Eldöntés tétele:
+# Tartalmaz-e 7-tel osztható számot a mátrix?
+feltétel = False
+számláló = 0    # Összehasonlításokat számoljuk (Hányszor fut le az if)
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        számláló += 1
+        if mtx[i][j] % 7 == 0:
+            feltétel = True
+            break # kilép az aktuális ciklusból
+    if feltétel:
+        break
+
+print(f"Összesen {számláló} elemet ellenőriztünk.") # 40 -> 24 -> 10
+if feltétel:
+    print("Van 7-tel osztható szám.")
+else:
+    print("Nincs 7-tel osztható szám")
