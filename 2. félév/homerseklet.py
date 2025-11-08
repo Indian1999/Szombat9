df49ee9ea791fe8f07d97925bb6fe4de45fe0089
Indian1999 <haszab99@gmail.com>
2025-11-08 10:30:05 +0100

    3. feladat

--- 2. félév/homerseklet.py
+++ 2. félév/homerseklet.py
@@ -34,14 +34,12 @@ for i in range(len(homerseklet)):
         összeg += homerseklet[i][j]
         if homerseklet[i][j] < homerseklet[i][min_index]:
             min_index = j
-        if homerseklet[i][j] > homerseklet[i][min_index]:
+        if homerseklet[i][j] > homerseklet[i][max_index]:
             max_index = j
     print(f"{i+1}. hét adatai:")
     print(f"\tÁtlag: {round(összeg / len(homerseklet[i]), 1)} °C")
     print(f"\tMinimum: {homerseklet[i][min_index]} °C")
     print(f"\tMaximum: {homerseklet[i][max_index]} °C")
-
-
 
 # 3. feladat: Írjuk ki, hogy milyen napon mennyi volt az átlag, min, max hőmérséklet!
 # pl.:
@@ -49,3 +47,23 @@ for i in range(len(homerseklet)):
 #       Átlag: 13.8
 #       min:    5.9
 #       max:   18.7
+
+napok = ["Hétfői", "Keddi", "Szerdai", "Csütörtöki", "Pénteki", "Szombati", "Vasárnapi"]
+for j in range(len(homerseklet[0])): # j: 0-6-ig
+    összeg = 0
+    min_index = 0
+    max_index = 0
+    for i in range(len(homerseklet)): # i: 0-51-ig
+        összeg += homerseklet[i][j]
+        if homerseklet[min_index][j] > homerseklet[i][j]:
+            min_index = i
+        if homerseklet[max_index][j] < homerseklet[i][j]:
+            max_index = i
+    print(napok[j], "napok adatai:")
+    print(f"\tÁtlag: {round(összeg / len(homerseklet), 1)}")
+    print(f"\tMinimum: {homerseklet[min_index][j]}")
+    print(f"\tMaximum: {homerseklet[max_index][j]}")
+    
+
+# 4. feladat: Melyik volt a legátlagosabb hét?
+# (az a hét ami átlag hőmérséklete a legközelebb van a teljes éves átlaghoz)
