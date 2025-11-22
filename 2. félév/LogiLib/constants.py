0411960ee89023165887ece77e93e577760a5b06
Indian1999 <haszab99@gmail.com>
2025-11-22 09:16:25 +0100

    consts

---
+++ 2. félév/LogiLib/constants.py
@@ -0,0 +1,4 @@
+pi = 3.14159265359
+tau = 2 * pi
+e = 2.71828
+nevek = ["András", "Bence", "Cecil", "Dénes", "Elemér", "Ferenc", "Gábor", "Hugó", "Izabella", "József", "Katalin", "László", "Mária", "Nóra", "Olivia", "Péter", "Róbert", "Szilveszter", "Tibor", "Viktória", "Zsuzsanna"]
