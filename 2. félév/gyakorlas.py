59d58aac0a5a1c06bf06f790944947bf9124ac25
Indian1999 <haszab99@gmail.com>
2025-12-14 10:19:15 +0100

    F

--- 2. félév/gyakorlas.py
+++ 2. félév/gyakorlas.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt # terminálba: pip install matplotlib
+
 # Írjunk egy olyan függvényt ami összeszorozza egy lista összes elemét
 
 def mult_list(lista):
@@ -35,7 +37,7 @@ map = [
     [".","#",".",".","^","#",".",".","#","."],
     ["#","#","#","#",".",".",".","#","#","."],
     ["#",".",".","#",".",".",".",".",".","."],
-    [".",".",".",".",".",".","#",".",".","."]
+    [".",".",".",".",".",".","#",".",".","F"]
     ]
 
 tavolsagok = [[float("inf") for j in range(len(map[i]))] for i in range(len(map))]
@@ -53,18 +55,24 @@ def find_distances(i, j):
     if j < len(tavolsagok[0]) - 1 and tavolsagok[i][j+1] < tavolsagok[i][j] + 1:
         tavolsagok[i][j] = tavolsagok[i][j+1] + 1
     # Megyünk tovább a szomszédos mezőkre
-    if i > 0 and map[i-1][j] == "." and tavolsagok[i-1][j] > tavolsagok[i][j] + 1:
+    if i > 0 and (map[i-1][j] == "." or map[i-1][j] == "F") and tavolsagok[i-1][j] > tavolsagok[i][j] + 1:
         find_distances(i-1, j)
-    if i < len(map) - 1 and map[i+1][j] == "." and tavolsagok[i+1][j] > tavolsagok[i][j] + 1:
+    if i < len(map) - 1 and (map[i+1][j] == "." or map[i+1][j] == "F") and tavolsagok[i+1][j] > tavolsagok[i][j] + 1:
         find_distances(i+1, j)
-    if j > 0 and map[i][j-1] == "." and tavolsagok[i][j-1] > tavolsagok[i][j] + 1:
+    if j > 0 and (map[i][j-1] == "." or map[i][j-1] == "F") and tavolsagok[i][j-1] > tavolsagok[i][j] + 1:
         find_distances(i, j-1)
-    if j < len(map[0]) - 1 and map[i][j+1] == "." and tavolsagok[i][j+1] > tavolsagok[i][j] + 1:
+    if j < len(map[0]) - 1 and (map[i][j+1] == "." or map[i][j+1] == "F") and tavolsagok[i][j+1] > tavolsagok[i][j] + 1:
         find_distances(i, j+1)
 
 # start_pos = (8, 7)
 # *start_pos = 8, 7    (*: kicsomagoló operátor)
 find_distances(*start_pos)
-for row in tavolsagok:
-    print(row)
     
+plt.imshow(tavolsagok)
+
+for i in range(len(tavolsagok)):
+    for j in range(len(tavolsagok[i])):
+        if tavolsagok[i][j] != float("inf"):
+            plt.text(j, i, tavolsagok[i][j], ha = "center", va = "center")
+
+plt.show()
