ed607f0fb00224bdd5735f5afa9f5f4e94d222dd
Indian1999 <haszab99@gmail.com>
2026-02-28 10:15:42 +0100

    feladatok

--- 3. félév/hibakezeles.py
+++ 3. félév/hibakezeles.py
@@ -39,3 +39,46 @@ except PermissionError:
     print("Ehez nincs jogom!")
 except:
     print("Egyéb hiba!")
+
+
+def fakt(n):
+    if not isinstance(n, int): # Ha n nem egy integer
+        raise TypeError(f"Érvénytelen argumentum típus ({type(n)}) (fakt({n})). Az argumentumnak egész számnak kell lennie!")
+    if n < 0:
+        raise ValueError(f"Érvénytelen argumentum érték ({n}). A fakt(n) függvény argumentuma kötelezően nemnegatív számnak kell lennie!")
+    if n == 0:
+        return 1
+    return n * fakt(n-1)
+
+try:
+    #print(fakt("cica")) # TypeError: unsupported operand type(s) for -: 'str' and 'int'
+    #print(fakt(3.5))
+    #print(fakt(-7))
+    print(fakt(1500))
+except TypeError as ex:
+    print(f"Típus hiba lépett fel a program futása során: {ex}")
+except ValueError as ex:
+    print(f"Érték hiba lépett fel: {ex}")
+except Exception as ex: # Általános/bármilyen hiba
+    print(f"Ismeretlen hiba: {type(ex)}, {ex}")
+
+
+# Saját kivétel típus létrehozása
+class CicaError(Exception):
+    def __init__(self, message):
+        super().__init__(f"{message}")
+
+raise CicaError("Hát ez egy cica hiba...")
+
+
+# 1. feladat: Írjunk egy függvényt amely paraméterben megkap egy évszámot.
+# És visszaadja, hogy azóta hány év telt el.
+# Ha jövőbeli évszámot adunk meg -> ValueError
+# Az egyáb lehetséges hibákat is kezeld le.
+
+# 1. b: Írj egy programot amely meghívja ezt a függvényt, és a lehetséges hibákat is lekezeli
+
+
+# 2. feladat: Írj egy függvényt ami megkap paraméterben egy iterálható objektumot (végig lehet menni rajta for ciklusssal)
+# A függvény adja vissza a paraméterben megkapott elemeinek az átlagát.
+# A függvény futása során esetlegesen fellépő problémákat kezeljük le!
