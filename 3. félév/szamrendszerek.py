c3b7c2c3f3653b2874592558108302ab41225aaf
Indian1999 <haszab99@gmail.com>
2026-03-28 10:26:42 +0100

    szamrednszerek2

--- 3. félév/szamrendszerek.py
+++ 3. félév/szamrendszerek.py
@@ -19,10 +19,14 @@ print(dec_to_bin(2315))
 
 
 def dec_to_base_n(num, n):
+    if n < 1 or n > 35:
+        raise ValueError("A cél számrendszer alapja 1 és 35 között kell, hogy legyen!")
     szamjegyek = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     output = ""
     if n == 1:
-        pass
+        for i in range(num):
+            output += "0"
+        return output
     while num != 0:
         output += szamjegyek[num % n]
         num = num // n
@@ -31,4 +35,36 @@ def dec_to_base_n(num, n):
 print(dec_to_base_n(155, 8)) # 233
 print(dec_to_base_n(155, 7)) # 311
 print(dec_to_base_n(155, 16))# 9B
-print(dec_to_base_n(155, 1))
+#print(dec_to_base_n(155, 160))
+
+# Feladat: Írjunk egy függvényt ami 2-es (str) számrendszerből vált 10-esbe
+
+def bin_to_dec(binary):
+    bin_reversed = binary[::-1]
+    összeg = 0
+    for i in range(len(bin_reversed)):
+        összeg += int(bin_reversed[i]) * 2**i
+    return összeg
+
+print(bin_to_dec("1100")) # 12
+
+
+def base_n_to_dec(num, n):
+    szamjegyek = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    num_reversed = num[::-1]
+    összeg = 0
+    for i in range(len(num_reversed)):
+        összeg += szamjegyek.index(num_reversed[i]) * n**i
+    return összeg
+
+print(base_n_to_dec("245", 8)) # 165
+print(base_n_to_dec("AF", 16)) # 175
+
+# Feladat: Írjunk egy függvényt, ami bármyilen számrendből átvált bármilyen számrendszerbe! (str)
+
+def base_n_to_base_m(num, n, m):
+    base10 = base_n_to_dec(num, n)
+    return dec_to_base_n(base10, m)
+
+print(base_n_to_base_m("14", 5, 3)) # 100
+print(base_n_to_base_m("FE17A9", 16, 2)) # 111111100001011110101001
